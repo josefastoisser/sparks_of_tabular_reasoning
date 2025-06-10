6b22e4e520560103822098aec1824c1bf8d0c19e
josefastoisser <118756833+josefastoisser@users.noreply.github.com>
2025-06-10 13:28:44 +0100

    Update serve_vllm.sh

--- evaluation/serve_vllm.sh
+++ evaluation/serve_vllm.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-nohup vllm serve Qwen/Qwen2.5-14B-Instruct \
+nohup vllm serve path/to/model \
     --dtype bfloat16 \
     --max-model-len 4096 \
     --tensor-parallel-size 1 \
-    > output4.log 2>&1 &
-
-
-#git push  https://josefastoisser:ghp_NF2MWZi9Aw9Xwuf2
-YevKjTzsjfpZ4d1p7Ucc@github.com/josefastoisser/sparks_of_tabular_reasoning.git
+    > output.log 2>&1 &
